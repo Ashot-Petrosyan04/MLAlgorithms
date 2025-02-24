f7e4ef499d6d79f22c02d5cdbeffdf9baa6757fb
Ashot-Petrosyan04 <84290763+Ashot-Petrosyan04@users.noreply.github.com>
2025-02-25 01:22:29 +0400

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,1 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import StandardScaler
 
+df = pd.read_csv('house_price_prediction_data.csv')
+
+binary_cols = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'preferred area (city center)']
+df[binary_cols] = df[binary_cols].replace({'yes': 1, 'no': 0})
+
+df = pd.get_dummies(df, columns=['furnishingstatus'], drop_first=True)
+
+X = df.drop('price', axis=1)
+y = df['price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = LinearRegression()
+model.fit(X_train_scaled, y_train)
+
+y_pred = model.predict(X_test_scaled)
+
+r_squared = r2_score(y_test, y_pred)
+
+n = X_test.shape[0]
+k = X_test.shape[1]
+adjusted_r_squared = 1 - (1 - r_squared) * (n - 1) / (n - k - 1)
