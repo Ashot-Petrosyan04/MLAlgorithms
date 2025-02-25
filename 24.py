a0ca37b75726feadbb08b6640ee93c3a13995f7d
Ashot-Petrosyan04 <84290763+Ashot-Petrosyan04@users.noreply.github.com>
2025-02-25 15:03:12 +0400

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,1 +1,17 @@
+import numpy as np
+from sklearn.datasets import make_circles
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
+X, y = make_circles(n_samples = 300, noise = 0.1, factor = 0.5, random_state = 42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+train_pred = model.predict(X_train)
+test_pred = model.predict(X_test)
+print(accuracy_score(y_train, train_pred))
+print(accuracy_score(y_test, test_pred))
